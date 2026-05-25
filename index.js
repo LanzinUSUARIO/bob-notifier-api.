784533840387e2420b73b19743b28a5213cc3530
LanzinUSUARIO <lanzin.silva.br@gmail.com>
2026-05-25 18:56:26 -0300

    fix site

--- index.js
+++ index.js
@@ -330,7 +330,7 @@ app.use((req, res, next) => {
         res.setHeader("Access-Control-Allow-Origin", origin);
         res.setHeader("Access-Control-Allow-Credentials", "true");
         res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
-        res.setHeader("Access-Control-Allow-Headers", "Content-Type,x-admin-pass,x-auth-token");
+        res.setHeader("Access-Control-Allow-Headers", "Content-Type,x-admin-pass,x-auth-token,Authorization");
     }
     if (req.method === "OPTIONS") return res.sendStatus(200);
     next();
